d7fa3b849263386099b2dc951e737d23fa9fa5a1
eiu165 <eiu165@gmail.com>
2014-05-27 23:17:07 -0500

    added notification service

---
+++ Source/Waterproofing/Infastructure/Business/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Waterproofing.Infastructure.Business
+{
+    public class NotificationService : INotificationService
+    { 
+        public void SendEmail(Entities.Prospect prospect, MailConfig mailConfig)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MailConfig
+    {
+        public string ToEmail { get; set; }
+        public string MailGunApiKey { get; set; }
+    }
+
+}
