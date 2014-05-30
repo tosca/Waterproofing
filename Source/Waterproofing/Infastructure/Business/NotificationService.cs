b3746ac8441598c8f26bb18023c3d928e9e7cd7a
eiu165 <eiu165@gmail.com>
2014-05-29 21:53:35 -0500

    send the message

--- Source/Waterproofing/Infastructure/Business/NotificationService.cs
+++ Source/Waterproofing/Infastructure/Business/NotificationService.cs
@@ -9,8 +9,53 @@ namespace Waterproofing.Infastructure.Business
     { 
         public void SendEmail(Entities.Prospect prospect, MailConfig mailConfig)
         {
-            throw new NotImplementedException();
+            mailConfig = new MailConfig { MailGunApiKey = GetMailGunApiKey(), ToEmail = GetEmailTo() };
+
+            var from = string.Format("{0} <{1}>", prospect.Name, "noreply@indbat.com");
+            var to = mailConfig.ToEmail;
+            var body = string.Format("Name:{0}<br /> Address:{1}<br /> City:{2}<br /> State:{3}<br /> Zip:{4}<br /> Phone:{5}<br /> Email:{6}<br /> Company:{7}<br /> Comments:{8}<br /> ",
+                prospect.Name, prospect.Address,
+                prospect.City, prospect.State, prospect.Zip, prospect.Phone, prospect.Email, prospect.Company, prospect.Comment);
+
+
+            RestClient client = new RestClient();
+            client.BaseUrl = "https://api.mailgun.net/v2";
+            client.Authenticator = new HttpBasicAuthenticator("api", mailConfig.MailGunApiKey);
+
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "app594.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", from);
+            request.AddParameter("to", to);
+            request.AddParameter("subject", "contact me");
+            request.AddParameter("text", body);
+            request.AddParameter("html", "<html>" + body + "</html>");
+            request.Method = Method.POST;
+            var response = client.Execute(request);
+            return;
         }
+
+
+
+        public static string GetMailGunApiKey()
+        {
+            var ret = "";
+            if (ConfigurationManager.AppSettings["MAILGUN_API_KEY"] != null)
+            {
+                ret = ConfigurationManager.AppSettings["MAILGUN_API_KEY"];
+            }
+            return ret;
+        }
+        public static string GetEmailTo()
+        {
+            var ret = "";
+            if (ConfigurationManager.AppSettings["EmailTo"] != null)
+            {
+                ret = ConfigurationManager.AppSettings["EmailTo"];
+            }
+            return ret;
+        }
+
     }
     public class MailConfig
     {
